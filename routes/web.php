466b1f68bf63557e0d480633b8ff6f70fbb9f419
ndhaka007 <nikhil.dhaka.bme14@itbhu.ac.in>
2019-08-03 00:55:31 +0530

    vote cater updated

--- routes/web.php
+++ routes/web.php
@@ -28,25 +28,25 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-route::get('/vote',function(){
+
+// to show cater
+route::get('/cater',function(){
+
+    $cater = cater::select('id','name','rating')->get();
+    return view('voteCater', compact('cater'));
+
+});
+
+//to vote for cater
+route::get('/vote/{id}',function($id){
     $user = auth()->user();
     $company_id = User::select('companyId')->where('id', $user)->get();
-    $cater_id = request('cater_id');
-    $vote = cater_voting::select('vote')->where('cater_id', $cater_id)->where('companyId', $company_id)->first();
-    $vote = $vote +1;
-    cater_voting::where('cater_id', $cater_id)->where('companyId', $company_id)->update(['vote'=>$vote]);
-    return "good";
+    $cater_id = $id;
+    $vote = cater_voting::where('caterId', $cater_id)->where('companyId', $company_id)->first();
+    $vote->vote = $vote->vote +1;
+    cater_voting::where('caterId', $cater_id)->where('companyId', $company_id)->update(['vote'=>$vote->vote]);
+
+    return redirect('/cater');
 
 });
 
-// to create cater
-route::post('/create', function(Request $request){
-
-    $cater = new cater();
-    $cater->name = request('name');
-    $cater->rating = request('rating');
-    $cater->count = request('count');
-
-    $cater->save();
-    return "good";
-});
